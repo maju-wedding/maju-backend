2048127aeca168dfc8fe1233f5edb7ccf20ce0f1
Jinwoo Hyun <jinwoo.hyun@prism39.com>
2025-02-13 18:06:10 +0900

    category

--- src/models/categories.py
+++ src/models/categories.py
@@ -4,6 +4,8 @@ from sqlmodel import Field, SQLModel
 class CategoryBase(SQLModel):
     name: str = Field(unique=True, index=True, max_length=20)
     display_name: str = Field(max_length=10)
+    is_ready: bool = Field(default=False)
+    order: int = Field(default=0, ge=0)
 
 
 class Category(CategoryBase, table=True):
@@ -13,11 +15,13 @@ class Category(CategoryBase, table=True):
 class CategoryCreate(SQLModel):
     name: str
     display_name: str
+    order: int
 
 
 class CategoryUpdate(SQLModel):
     name: str | None = None
     display_name: str | None = None
+    order: int | None = None
 
 
 class CategoryRead(CategoryBase):

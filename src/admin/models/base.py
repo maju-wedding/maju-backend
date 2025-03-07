e33c140d194e82d2ff17c2e02f9ef8a7dd6bdb60
PrimadonnaGit <jwhyun2215@gmail.com>
2025-03-08 00:29:02 +0900

    fix: 어드민 성능 개선

--- src/admin/models/base.py
+++ src/admin/models/base.py
@@ -1,4 +1,5 @@
 from sqladmin import ModelView
+from sqlalchemy.orm import joinedload
 from sqlmodel import select
 
 
@@ -8,13 +9,29 @@ class BaseModelViewWithFilters(ModelView):
     page_size = 10
     page_size_options = [10, 20, 50, 100]
 
+    relation_prefixes = {
+        "product.": "product",
+        "checklist_category.": "checklist_category",
+        "enterprise.": "enterprise",
+        "user.": "user",
+        # 필요한 다른 관계들 추가
+    }
+
     def list_query(self, request):
         query = select(self.model)
 
-        page = request.query_params.get("page")
-        page_size = request.query_params.get("page_size")
+        used_relations = set()
+        for col in self.column_list:
+            if isinstance(col, str):
+                for prefix, attr in self.relation_prefixes.items():
+                    if col.startswith(prefix):
+                        used_relations.add(attr)
+                        break
 
-        # 삭제 상태 필터
+        for relation in used_relations:
+            query = query.options(joinedload(getattr(self.model, relation)))
+
+        # Filter
         is_deleted_param = request.query_params.get("is_deleted")
         if is_deleted_param == "true":
             query = query.where(self.model.is_deleted == True)
@@ -23,11 +40,10 @@ class BaseModelViewWithFilters(ModelView):
         elif is_deleted_param is None:
             query = query.where(self.model.is_deleted == False)
 
-        query = query.order_by(self.model.id)
-
-        if page:
-            query = query.offset((int(page) - 1) * self.page_size)
-        if page_size:
-            query = query.limit(int(page_size))
+        # Pagination
+        page = request.query_params.get("page", 1)
+        page_size = request.query_params.get("page_size", self.page_size)
+        query = query.offset((int(page) - 1) * self.page_size)
+        query = query.limit(int(page_size))
 
         return query
--- src/core/db.py
+++ src/core/db.py
@@ -1,12 +1,12 @@
-from typing import Any
 from collections.abc import AsyncGenerator
+from typing import Any
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.asyncio.session import async_sessionmaker
 
 from core.config import settings
 
-async_engine = create_async_engine(settings.DATABASE_URI, echo=False, future=True)
+async_engine = create_async_engine(settings.DATABASE_URI, echo=True, future=True)
 
 async_session = async_sessionmaker(
     bind=async_engine, class_=AsyncSession, expire_on_commit=False
